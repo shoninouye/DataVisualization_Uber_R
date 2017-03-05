c50d7ebb95747f1af95e81e09cb812c836a66791
Shon Inouye <shoninouye@gmail.com>
2017-03-05 14:53:40 -0800

    Update uber_data_visualization.R

--- uber_data_visualization.R
+++ uber_data_visualization.R
@@ -1,8 +1,6 @@
 library(ggplot2)
 library(plotly)
 library(data.table)
-library(stats)
-library(ISLR)
 library(ggmap)
 library(plyr)
 library(dplyr)
@@ -19,22 +17,40 @@ apr14_plot
 nyMap <- qmap("New York City")
 
 nyMap + 
-  geom_point(data = uber_apr14, mapping = aes(x = Lon, y = Lat), colour = 'red',
-             alpha = 0.5, na.rm = TRUE)
+  geom_point(data = uber_apr14, mapping = aes(x = Lon, y = Lat), colour = 'blue',
+             size = 0.1, alpha = 0.5, na.rm = TRUE)
 
 main_plot <- nyMap + 
   geom_point(data = uber_apr14, 
              mapping = aes(x = Lon, y = Lat,  colour = factor(uber_apr14$Base)), 
-             alpha = 0.4, na.rm = TRUE) +
+             size = 0.1, alpha = 0.4, na.rm = TRUE) +
   scale_color_manual(values = c("#008080", "#7F462c", "#FF2400", "#2B60DE", "#CA226B"))
 #COLOR:
 # + scale_fill_manual(values = c("__", "___", "___"))
 
+### SEPARATING DATA BASED ON UBER BASE ###
 uber_baseB02512 <- filter(uber_apr14, Base == "B02512")
 uber_baseB02598 <- filter(uber_apr14, Base == "B02598")
 uber_baseB02617 <- filter(uber_apr14, Base == "B02617")
 uber_baseB02682 <- filter(uber_apr14, Base == "B02682")
 uber_baseB02764 <- filter(uber_apr14, Base == "B02764")
+
+## CREATE BAR GRAPH OF UBER DATA BY BASE ###
+
+numrow_B02512 <- nrow(uber_baseB02512)
+numrow_B02598 <- nrow(uber_baseB02598)
+numrow_B02617 <- nrow(uber_baseB02617)
+numrow_B02682 <- nrow(uber_baseB02682)
+numrow_B02764 <- nrow(uber_baseB02764)
+
+basedata <- [graph_objs.Bar(x = [B02512, B02598, B02617, B02682, B02764],
+                            y = [numrow_B02512, numrow_B02598, numrow_B02617, numrow_B02682, numrow_B02764])
+             ]
+base_length_list <- c(numrow_B02512, numrow_B02598, numrow_B02617, numrow_B02682, numrow_B02764)
+
+barplot(base_length_list)
+
+### PLOT DATA BY UBER BASE ###
 
 #Base B02512
 base_B02512 <- nyMap + 
@@ -71,7 +87,6 @@ base_B02764 <- nyMap +
              alpha = 0.4, na.rm = TRUE) +
   scale_color_manual(values = c("#CA226B"))
 
-par(mfrow=c(2,3))
 plot(main_plot)
 plot(base_B02512)
 plot(base_B02598)
@@ -81,7 +96,7 @@ plot(base_B02764)
 
 ggplot(data = uber_apr14, 
            aes(x = Lon, y = Lat,  colour = factor(uber_apr14$Base))) +
-  geom_point(alpha = 0.4, na.rm = TRUE) +
+  geom_point(size = 0.1, alpha = 0.4, na.rm = TRUE) +
   scale_color_manual(values = c("#008080", "#7F462c", "#FF2400", "#2B60DE", "#CA226B")) + 
   facet_wrap(~ Base) +
   theme_bw()
